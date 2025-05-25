d2b858b4c5b0f7cc2d962694bc6244d7badeb994
Mad0Squirrel <eghorkin@gmail.com>
2025-05-25 15:25:58 +0300

    feat: добавлена функция исправления имени и фамилии

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import re
+
+def fix_name(name):
+    match = re.fullmatch(r"([А-ЯЁа-яё]+)\s?([А-ЯЁа-яё]+)", name.strip())
+    if match:
+        return f"{match.group(1).capitalize()} {match.group(2).capitalize()}"
+    return ""
+
 with open("input.txt", "r", encoding="utf-8") as fin, open("output.txt", "w", encoding="utf-8") as fout:
     for line in fin:
         fout.write(line)
